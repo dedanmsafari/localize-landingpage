88e1b0ffd74e99e3731a1497e5e537764897a51d
dedanmsafari <dedan.msafari@gmail.com>
2023-01-12 16:15:16 +0300

    browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,6 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+import RootWrapper from "./src/components/ui/root-wrapper"
+
+export const wrapRootElement = RootWrapper
